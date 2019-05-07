067b307da55d402b47142d7f900bc68a64f764f5
USAMA KHALID <usamathescientist@gmail.com>
2019-05-07 19:29:43 +0500

    Initial Changes

--- App.js
+++ App.js
@@ -7,43 +7,56 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Image, Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const bloodRed = 'red';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container:{
+        flex:1,
+        justifyContent:'center',
+        padding: 10
+    },
+    input: {
+        backgroundColor: bloodRed,
+        color: 'white',
+        height: 40,
+        borderRadius:15
+    }
 });
+
+export default class SignIn extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    style={{width:300,height: 300}}
+                    resizeMode="contain"
+                    source={require('./logo.png')}
+                />
+                <TextInput
+                    style = {styles.input}
+                    placeholder="Email or Username"
+                    placeholderTextColor = 'white'/>
+                <TextInput
+                    style = {styles.input}
+                    placeholder="Password"
+                    placeholderTextColor = 'white'
+                    secureTextEntry={true}/>
+                <Button
+                    color={bloodRed}
+                    title="Log in"
+                />
+
+                <Text style={{color: bloodRed}}>Forgot Password?</Text>
+                <Text style={{color: bloodRed}}>OR</Text>
+
+                <Button
+                    color={bloodRed}
+                    title="Register"
+                />
+
+            </View>
+        );
+    }
+}

f5fc64cfd54be160b272769f9b77d9e35a4ed83b
USAMA KHALID <usamathescientist@gmail.com>
2019-05-09 22:14:46 +0500

    Sign in page

--- App.js
+++ App.js
@@ -6,57 +6,103 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Image, Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, { Component } from 'react';
+import { Image, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { createStackNavigator, createAppContainer} from 'react-navigation';
 
-const bloodRed = 'red';
+const bloodRed = '#D23E3E';
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        padding: 10
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        padding: 10,
     },
     input: {
         backgroundColor: bloodRed,
         color: 'white',
         height: 40,
-        borderRadius:15
-    }
+        borderRadius: 15,
+        padding: 10,
+        marginBottom: 10,
+    },
+    text: {
+        color: bloodRed,
+        alignSelf: 'center',
+        margin: 10,
+    },
 });
 
-export default class SignIn extends Component {
+class SignInScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
                 <Image
-                    style={{width:300,height: 300}}
+                    style={{ flex: 1, alignSelf: 'center' }}
                     resizeMode="contain"
                     source={require('./logo.png')}
                 />
-                <TextInput
-                    style = {styles.input}
-                    placeholder="Email or Username"
-                    placeholderTextColor = 'white'/>
-                <TextInput
-                    style = {styles.input}
-                    placeholder="Password"
-                    placeholderTextColor = 'white'
-                    secureTextEntry={true}/>
-                <Button
-                    color={bloodRed}
-                    title="Log in"
-                />
+                <View style={styles.container}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email or Username"
+                        placeholderTextColor="white"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                        placeholderTextColor="white"
+                        secureTextEntry={true}
+                    />
+                    <Button color={bloodRed} title="Log in" />
 
-                <Text style={{color: bloodRed}}>Forgot Password?</Text>
-                <Text style={{color: bloodRed}}>OR</Text>
+                    <Text
+                        style={[
+                            styles.text,
+                            { fontSize: 15, textDecorationLine: 'underline' },
+                        ]}>
+                        Forgot Password?
+                    </Text>
+                </View>
 
+                <Text style={[styles.text, { fontSize: 20 }]}>OR</Text>
                 <Button
                     color={bloodRed}
                     title="Register"
-                />
-
+                    onPress={() =>
+                        this.props.navigation.navigate('Register' )
+                    } />
             </View>
         );
     }
 }
+
+class RegisterScreen extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Details Screen</Text>
+            </View>
+        );
+    }
+}
+
+const AppNavigator = createStackNavigator(
+    {
+        SignIn: {
+            screen: SignInScreen,
+            navigationOptions: { header: null }
+        },
+        Register: {
+            screen: RegisterScreen,
+            navigationOptions:{ header: null }
+        },
+    },
+    {
+        initialRouteName: 'SignIn'
+    }
+);
+
+const App = createAppContainer(AppNavigator);
+export default App;
